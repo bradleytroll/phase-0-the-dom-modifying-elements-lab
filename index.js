ff614c3cb431da2052d1ef6092e988897bbc53d7
Bradley <bradleytroll@gmail.com>
2023-04-07 17:37:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const mainElement = document.querySelector('#main');
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'Bradley is the champion!';
+document.body.appendChild(newHeader);
